aa4df3b74f9923ee3ad87fa691a23de1c05a1578
JosielOliveira <joliviera.prof@gmail.com>
2024-11-13 22:49:24 +0100

    refresh

---
+++ Poke-Proyecto/components/login/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    if (loginForm) {
+        loginForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+
+            // Usuario y contraseña de admin
+            const validUsername = 'admin';
+            const validPassword = '1234';
+
+            if (username === validUsername && password === validPassword) {
+                // Guardar el estado de login en localStorage
+                localStorage.setItem('loggedIn', 'true');
+                // Redirigir a index.html
+                window.location.href = 'index.html';
+            } else {
+                // Mostrar mensaje de error
+                document.getElementById('error-message').style.display = 'block';
+            }
+        });
+    }
+});
